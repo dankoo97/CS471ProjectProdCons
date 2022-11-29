44178ab7a53626d44f50d02b8f1b824b87aa7cb5
dankoo97 <dak.7997@gmail.com>
2022-11-29 14:02:52 -0500

    Changes by Josh

--- src/main/java/prodcon/Producer.java
+++ src/main/java/prodcon/Producer.java
@@ -1,4 +1,51 @@
 package prodcon;
 
-public class Producer {
+import prodcon.buffer.Buffer;
+import prodcon.buffer.SaleRecord;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Producer extends Thread {
+    private int storeID;
+    private static final Semaphore createItem = new Semaphore(1);
+
+    public Producer() {
+        storeID = 0;
+    }
+
+    public Producer(int storeID){
+        this.storeID = storeID;
+    }
+
+    public void generateItems() throws InterruptedException {
+        while (true) {
+            createItem.acquire();
+            if (SaleRecord.getTotalNumberOfSales() < 1000) {
+                SaleRecord saleRecord = SaleRecord.randomSaleRecord(this.storeID);
+                Buffer.addSaleRecord(saleRecord);
+                createItem.release();
+            } else {
+                createItem.release();
+                break;
+            }
+
+            Thread.sleep(ThreadLocalRandom.current().nextInt(5, 40));
+        }
+    }
+
+    public int getStoreID() {
+        return storeID;
+    }
+
+    public void setStoreID(int storeID){ this.storeID = storeID;}
+
+    @Override
+    public void run() {
+        try {
+            generateItems();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
