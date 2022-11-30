9e6a2492bcd61099093233ce1cfaf8d7520583b7
Jpete020 <98491629+Jpete020@users.noreply.github.com>
2022-11-30 12:27:07 -0500

    ProdConDriver documentation added

--- src/main/java/prodcon/ProdConDriver.java
+++ src/main/java/prodcon/ProdConDriver.java
@@ -20,22 +20,35 @@ public class ProdConDriver {
         ArrayList<Producer> producers = new ArrayList<>(p);
         ArrayList<Consumer> consumers = new ArrayList<>(c);
 
+        /**
+         * Loops p times, generating p producers
+         */
         for (int i = 0; i < p; i++) {
             producers.add(new Producer(i+1));
             producers.get(i).start();
         }
 
+        /**
+         * Loops c times, generating c consumers
+         */
         for (int i = 0; i < c; i++) {
             consumers.add(new Consumer());
             consumers.get(i).start();
         }
 
+        /**
+         * Listening for producer threads to finish execution
+         */
         for (Producer producer : producers) {
             producer.join();
         }
 
         Statistics globalStats = new Statistics();
 
+        /**
+         * Listens for consumer threads to finish execution
+         * then prints consumer statistics
+         */
         for (Consumer consumer : consumers) {
             consumer.join();
             Statistics consumerStats = consumer.getStatistics();
